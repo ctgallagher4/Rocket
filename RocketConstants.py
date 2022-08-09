794ee6f3d4ef010ab837360c8fc72e266539586f
ctgallagher4 <ctgall4@gmail.com>
2022-08-09 16:47:26 -0400

    added collisions

--- RocketConstants.py
+++ RocketConstants.py
@@ -2,7 +2,10 @@ import numpy as np
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-MAX_SPEED = 5
+MAX_SPEED = 7
+ACC_FACTOR = 10
 display_width = 1800
 display_height = 1200
 R_CON = (2 * np.pi) / 360
+ASTEROID_MIN_SIZE = 50
+ASTEROID_MAX_SIZE = 100
--- RocketModule.py
+++ RocketModule.py
@@ -1,11 +1,35 @@
-from turtle import window_height, window_width
 import pygame
 import numpy as np
 from RocketConstants import *
 
-
-
 class Game:
+
+    @staticmethod
+    def checkCollision(asteroidSystem, missileSystem, rocket):
+
+        masterSystem = []
+
+        masterSystem.extend(asteroidSystem.system)
+        masterSystem.extend(missileSystem.system)
+        masterSystem.append(rocket)
+
+        for item1 in masterSystem:
+            for item2 in masterSystem:
+                if item1 != item2:
+                    min_dist = item1.RADIUS + item2.RADIUS 
+                    dist_actual = np.sqrt((item1.x - item2.x) ** 2 + (item1.y - item2.y) ** 2)
+                    if dist_actual <=  min_dist:
+                        if rocket == item1 or rocket == item2:
+                            return True
+                        else:
+                            asteroidSystem.delete(item1)
+                            asteroidSystem.delete(item2)
+                            missileSystem.delete(item1)
+                            missileSystem.delete(item2)
+
+        
+        return False
+
     @staticmethod
     def run():
         
@@ -17,7 +41,7 @@ class Game:
             #sprite initialization
         missileSystem = MissileSystem()
         rocket = Rocket(800, 600, missileSystem, SURFACE)
-        asteroid = Asteroid(500, 400, 4, 4, SURFACE)
+        asteroidSystem = AsteroidSystem(SURFACE)
     
         count = 0
         gameOn = True
@@ -32,7 +56,11 @@ class Game:
 
             rocket.update(count, SURFACE)
             missileSystem.update()
-            asteroid.update()
+            asteroidSystem.update()
+
+
+            if Game.checkCollision(asteroidSystem, missileSystem, rocket):
+                gameOn = False
 
             clock.tick(30)
             pygame.display.update()
@@ -46,11 +74,78 @@ class CircleObject:
         self.x = x
         self.y = y
         self.xVel = 0
-        self.yVel = 0 
+        self.yVel = 0
+
+class AsteroidSystem(CircleObject):
+
+    system = []
+
+    def __init__(self, SURFACE):
+        self.SURFACE = SURFACE
+
+    def draw(self):
+        for i in self.system:
+            i.draw()
+
+    def update(self):
+        if len(self.system) < 5:
+            
+            listo = []
+
+            #topstart
+            xStart = np.random.randint(0, display_width)
+            yStart = 0
+            listo.append((xStart, yStart))
+            #bottomstart
+            xStart = np.random.randint(0, display_width)
+            yStart = display_height
+            listo.append((xStart, yStart))
+            #leftstart
+            xStart = 0
+            yStart = np.random.randint(0, display_height)
+            listo.append((xStart, yStart))
+            #rightstart
+            xStart = display_width
+            yStart = np.random.randint(0, display_height)
+            listo.append((xStart, yStart))
+
+            choice = np.random.choice(range(0,len(listo)))
+
+            xSpeed = np.random.randint(-1 * MAX_SPEED, MAX_SPEED/1.5)
+            ySpeed = np.random.randint(-1 * MAX_SPEED, MAX_SPEED/1.5)
+
+            self.system.append(Asteroid(*listo[choice], xSpeed, ySpeed, self.SURFACE))
+
+        for asteroid in self.system:
+            asteroid.update()
+        
+        self.draw()
+
+    def delete(self, item):
+        if item in self.system:
+
+            R = item.RADIUS
+
+            if item.RADIUS > 25:
+
+                S = self.SURFACE
+
+                a1Speed = (np.random.randint(0,5), np.random.randint(0,5))
+                a2Speed = (np.random.randint(0,5), -1 * np.random.randint(0,5))
+                a3Speed = (-1 * np.random.randint(0,5), np.random.randint(0,5))
+                a4Speed = (-1 * np.random.randint(0,5), -1 * np.random.randint(0,5))
+                
+                a1 = Asteroid(item.x + R, item.y + R, *a1Speed, S, 20)
+                a2 = Asteroid(item.x + R, item.y - R, *a2Speed, S, 20)
+                a3 = Asteroid(item.x - R, item.y + R, *a3Speed, S, 20)
+                a4 = Asteroid(item.x - R, item.y - R, *a4Speed, S, 20)
+
+                self.system.extend([a1, a2, a3, a4])
+
+            self.system.remove(item)
+
 
 class Asteroid(CircleObject):
-
-    RADIUS = np.random.randint(50, 100)
 
     def generatePoints(self):
 
@@ -59,7 +154,7 @@ class Asteroid(CircleObject):
         x = self.x
         y = self.y
         rad = self.RADIUS
-        thetas = np.linspace(0 , 2 * np.pi , 10)
+        thetas = np.linspace(0 , 2 * np.pi , 9)
         xRands = [-1 * np.random.randint(0, rad/5) for i in range(len(thetas))]
         yRands = [-1 * np.random.randint(0, rad/5) for i in range(len(thetas))]
         xs = (self.RADIUS * np.cos(thetas)) + x
@@ -71,19 +166,21 @@ class Asteroid(CircleObject):
 
         return list(points)
         
-    def __init__(self, x, y, xVel, yVel, SURFACE):
+    def __init__(self, x, y, xVel, yVel, SURFACE, radius=None):
         super().__init__(x, y)
         self.xVel = xVel
         self.yVel = yVel
         self.SURFACE = SURFACE
         self.SEED = np.random.randint(0, 100) # 100 possible asteroids
+        if radius == None:
+            self.RADIUS = np.random.randint(ASTEROID_MIN_SIZE, ASTEROID_MAX_SIZE)
+        else:
+            self.RADIUS = radius
         self.points = self.generatePoints()
 
  
 
     def draw(self):
-
-        
         
         pygame.draw.polygon(self.SURFACE, WHITE, self.points, 2)
 
@@ -97,8 +194,8 @@ class Asteroid(CircleObject):
 
         self.points = self.generatePoints()
 
-        self.x = self.x % display_width
-        self.y = self.y % display_height
+        self.x = self.x % (display_width)
+        self.y = self.y % (display_height)
         
         
 
@@ -137,19 +234,23 @@ class Missile(CircleObject):
 
 class MissileSystem:
 
-    missiles = []
+    system = []
 
     def update(self):
 
         missiles = []
-        for missile in self.missiles:
+        for missile in self.system:
             
             missile.update()
 
             if missile.x < display_width and missile.x > 0:
                 missiles.append(missile)
 
-        self.missiles = missiles
+        self.system = missiles
+
+    def delete(self, item):
+        if item in self.system:
+            self.system.remove(item)
 
 class Rocket(CircleObject):
 
@@ -205,12 +306,14 @@ class Rocket(CircleObject):
     
         if keys[pygame.K_SPACE] == 1 and count % 7 == 0:
             tipLoc = (self.x + self.RADIUS * np.cos(self.tip * R_CON), 
-                  self.y + self.RADIUS * np.sin(self.tip * R_CON))
-            missXVel = np.cos(self.tip * R_CON)/self.RADIUS * 1000 + self.xVel
-            missYVel = np.sin(self.tip * R_CON)/self.RADIUS * 1000 + self.yVel
-            newMissile = Missile(*tipLoc, missXVel, missYVel, self.tip, SURFACE)
+                      self.y + self.RADIUS * np.sin(self.tip * R_CON))
+            missXVel = np.cos(self.tip * R_CON)/self.RADIUS *1000
+            missYVel = np.sin(self.tip * R_CON)/self.RADIUS *1000
+            
+            newMissile = Missile(tipLoc[0], tipLoc[1], missXVel, missYVel, self.tip, SURFACE)
+            
 
-            self.missileSystem.missiles.append(newMissile)
+            self.missileSystem.system.append(newMissile)
 
 
     def update(self, count, SURFACE):
@@ -222,8 +325,8 @@ class Rocket(CircleObject):
         
         self.tip -= (keys[pygame.K_a] - keys[pygame.K_d]) * 8
         tip = self.tip
-        self.xVel += np.cos(tip * R_CON) * keys[pygame.K_w]/self.RADIUS * 6
-        self.yVel += np.sin(tip * R_CON) * keys[pygame.K_w]/self.RADIUS * 6
+        self.xVel += np.cos(tip * R_CON) * keys[pygame.K_w]/self.RADIUS * ACC_FACTOR
+        self.yVel += np.sin(tip * R_CON) * keys[pygame.K_w]/self.RADIUS * ACC_FACTOR
 
         self.checkSpeed()
 
